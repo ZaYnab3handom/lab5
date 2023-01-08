5a305c412f9e8ef181de69953883f7d5da9fdc3a
ZaYnab <zaynab3ibrahim@gmail.com>
2023-01-08 19:35:49 +0200

    first commit

---
+++ js.js
@@ -0,0 +1,6 @@
+/*-	In the login form assignment in Day.6 lab, use local storage to save user name and password in the prev. assignment, 
+  when user press login button, and retrieve them on page load and write them to the textboxes.
+    a.	Can you use session storage in the previous assignment? Why?
+    b.	What’s the difference between local storage and session storage?
+    c.	Can you access local storage and session storage in another page than the page that created it (on the same site)?
+ */
